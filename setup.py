6de012b1b127078510d2441634d319d87ae56032
Richard Tia <richie.tia@gmail.com>
2023-02-16 12:00:50 -0800

    chore: upload to PyPI

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="substrait-tools-rtia86",
+    name="substrait-tools",
     version="0.0.1",
     author="richtia",
     description="A Substrait command line tool",
